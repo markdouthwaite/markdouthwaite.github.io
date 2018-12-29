dbe4ab251900bf59d249d44e59bc92b9120e8ad6
MarkD <mark.l.douthwaite@gmail.com>
2018-12-29 22:26:57 +0000

    New post draft

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"This series of posts is going to give an overview of some interesting, useful and/or time-saving general-purpose packages that can make the lives of Data Scientists a little easier. I thought I’d put this together after finding a few-to-many DS repositories, scripts and notebooks that made life far too difficult...","categories": [],
         "tags": ["data science","python"],
         "url": "http://localhost:4000/NonDataSciencePackagesForDataSciencept1tqdm/",
+        "teaser":null},{
+        "title": "Reading List 2018",
+        "excerpt":"A Quick(ish) Word About Books I think there is something profoundly healthy and ‘enlightening’ about reading long-form content, both fiction and non-fiction, that is not present in short-form content like many blogs (the irony, I know!), news articles and, to some extent, academic papers. I worry – maybe wrongly –...","categories": [],
+        "tags": ["books","education"],
+        "url": "http://localhost:4000/ReadingList2018/",
         "teaser":null}]
